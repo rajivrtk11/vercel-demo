7be81bdd30bc4df3f938bdfbb60764dea95119cc
Apple <apple@Apples-MacBook-Pro.local>
2023-02-03 11:37:32 +0530

    added user path

--- index.js
+++ index.js
@@ -6,6 +6,25 @@ app.use("/", (req, res) => {
   res.json({ message: "Hello From Express App" });
 });
 
+app.use("/get-user", (req, res) => {
+  res.json({ 
+    user: [
+      {
+        "name":'Rajiv',
+        "phone": 9729188851
+      },
+      {
+        "name":'Sanjiv',
+        "phone": 9729188851
+      },
+      {
+        "name":'Rajiv',
+        "phone": 9729188851
+      }
+    ],
+    message: "Hello From Express App"
+  });
+});
 app.listen(9000, () => {
   console.log(`Starting Server on Port ${port}`);
 });
